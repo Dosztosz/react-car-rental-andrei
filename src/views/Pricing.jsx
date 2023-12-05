13d708f52deb94275214366e295f51143138513c
Dosztosz <dosztosz@int.pl>
2023-12-05 12:07:21 +0100

    up

--- src/views/Pricing.jsx
+++ src/views/Pricing.jsx
@@ -3,7 +3,15 @@ import { useState } from "react";
 import { PricingCard } from "../components";
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return <div className="mt-36">
+    <div className="wrapper">
+      <h1 className="head_text text-center"> See our <span className="green_text">pricing plan</span></h1>
+      <div className="py-9 flex items-center justify-center gap-7">
+        <p className={`font-bold ${}`}></p>
+      </div>
+    </div>
+    
+  </div>;
 };
 
 export default Pricing;
