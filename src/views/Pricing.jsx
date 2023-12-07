c2788928d2dfee024ecdd30e775bcb04ca4e8f63
Dosztosz <dosztosz@int.pl>
2023-12-07 11:27:47 +0100

    pricing updated

--- src/views/Pricing.jsx
+++ src/views/Pricing.jsx
@@ -16,13 +16,32 @@ const Pricing = () => {
           } trnsition-all duration-300 ease-in`}>
             Monthly
           </p>
-          <label className="relative infline-flex items-center mb-r cursor-pointer">
+          <label className="relative infline-flex items-center cursor-pointer">
         <input type="checkbox" className="sr-only peer" value={switchVal} onChange={() => setSwitchVal((prev) => !prev)}/>
         <div className="w-11 h-6 bg-gray-200 rounded-full peer
                         dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white
                         after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full
                         after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-green"/>
       </label>
+
+      <p className={`font-bold ${
+          switchVal ? "text-primary-green" : "text-primary-black"
+          } trnsition-all duration-300 ease-in`}>
+            Annually
+          </p>
+
+      </div>
+
+      <div className="flex flex-col xl:flex-row items-center gap-5">
+        <div className="flex flex-col sm:flex-row gap-5 flex-initial xl:flex-1 w-full justify-start sm:justify-center items-center sm:items-start">
+          <PricingCard special headText="Save by choosing our best plan"/>
+          <PricingCard plan="essential" headText={`$ ${switchVal ? "349.99" : "29.50" }`}/>
+        </div>
+
+        <div className="flex flex-col sm:flex-row gap-5 flex-initial xl:flex-1 w-full justify-start sm:justify-center items-center sm:items-start">
+          <PricingCard plan="recommended" headText={`$ ${switchVal ? "529.99" : "44.50" }`}/>
+          <PricingCard plan="pro" headText={`$ ${switchVal ? "845.50" : "70.50" }`}/>
+        </div>
       </div>
 
     </div>
